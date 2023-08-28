d79a419022c0a3c62e69ed4f750b6dfd652c987a
Simon Vargas <sjvargas01@gmail.com>
2023-08-28 16:47:40 -0400

    created kafkaproducer config

--- src/main/java/com/example/kafkaexampleapp/config/KafkaProducerConfig.java
+++ src/main/java/com/example/kafkaexampleapp/config/KafkaProducerConfig.java
@@ -3,8 +3,10 @@ package com.example.kafkaexampleapp.config;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
 import java.util.HashMap;
@@ -23,9 +25,14 @@ public class KafkaProducerConfig {
 
         return props;
     }
-
+    @Bean
     public ProducerFactory<String, String> producerFactory() {
         return new DefaultKafkaProducerFactory<>(producerConfig());
-
+    }
+    @Bean
+    public KafkaTemplate<String, String> stringKafkaTemplate(
+            ProducerFactory<String, String> producerFactory
+    ) {
+        return new KafkaTemplate<>(producerFactory);
     }
 }
