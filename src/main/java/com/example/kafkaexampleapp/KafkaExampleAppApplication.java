a38a099c095effe2449e1519b33d71fa36c83ac3
Simon Vargas <sjvargas01@gmail.com>
2023-08-28 16:27:36 -0400

    first commit

---
+++ src/main/java/com/example/kafkaexampleapp/KafkaExampleAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaexampleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaExampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaExampleAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kafkaexampleapp/KafkaExampleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkaexampleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaExampleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
